0ae69932d2af2e20c153093a1b351a66fca80489
Abbadis <pustovoydima@gmail.com>
2020-08-01 22:07:29 +0300

    add margin in small screens

--- filtr.js
+++ filtr.js
@@ -106,6 +106,10 @@ function StartFilter(n){
 
 		if(document.body.clientWidth <= 500){
 			document.getElementById('works__types__list').style.width = "90%";
+			for(let i = 0;  i < elem.length; i++){
+				elem[i].style.margin ="5px";
+				elem[i].style.animation = "opacity__move 2s ";
+			}
 		}
 	}
 	
